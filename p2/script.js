89b25d042bc03c418f77ca8c49da47fe3f27c8b7
farhan khan <khan0397@algonquinlive.com>
2020-01-13 16:46:15 -0500

    final calculations made

--- p2/script.js
+++ p2/script.js
@@ -2,10 +2,11 @@ let name = 'Farhan'
 console.log('Hello my name is ${Farhan}.')
 
 let number = 5
+let product = 0
 
-    console.log(`The number ${number} multiply by 2 is ${ number * 2 }.`)
-    console.log(`The number multiply by 5 is ${ product * 5 () }.`)
-    console.log(`The result divided by 5 is ${ product * 5 () }./ number.`)
-    let finalValue = { product,  *5() } / number
-    console.log(`The result minus 7 is' ${ finalValue } - 7`)
+    console.log(`The number ${number} multiply by 2 is ${product = number * 2 }.`)
+    console.log(`The number multiply by 5 is ${ product * 5 }.`)
+    console.log(`The result divided by 5 is ${ product * 5 / number}.`)
+    let finalValue =  product * 5  / number
+    console.log(`The result minus 7 is ${ finalValue - 7 }`)
 
