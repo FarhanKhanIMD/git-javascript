c1d9df9186e0337fa6cf71b9671c6ddfe0134add
farhan khan <khan0397@algonquinlive.com>
2020-01-13 16:03:20 -0500

    Updating

--- p2/script.js
+++ p2/script.js
@@ -2,8 +2,9 @@ let name = 'Farhan'
 console.log('Hello my name is ${Farhan}.')
 
 let number = 5
+let result = 0
 
-console.log('The number $ {number} multiply by 2 is' ${ number * 2}.)
+console.log('The number $ {number} multiply by 2 is' $ { number * 2.}) 
 console.log ('The number multiply by 5 is' $ {product * 5}.)
 console.log('The result divided by 5 is' $ {product * 5}. / number.)
 let finalValue = {product * 5} / number
